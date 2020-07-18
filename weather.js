4eebe29176794de7db932928899edd9520268ddd
edcyhnujmi <43102478+edcyhnujmi@users.noreply.github.com>
2020-07-18 20:39:04 +0900

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,17 +1,17 @@
 const weather = document.querySelector(".js-weather");
 
-const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";/////
+const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
 const COORDS = 'coords';
 
-function getWeather(lat,lng){/////
+function getWeather(lat,lng){
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`).then(function(response) {
         return response.json();
-    }).then(function(json) { //then 사용하면 데이터 준비될 때까지 기다리게 할 수 있음
+    }).then(function(json) {
         const temperature = json.main.temp;
         const place = json.name;
         weather.innerText = `${temperature} @ ${place}`;
-    });/////
-}/////
+    });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -21,17 +21,11 @@ function handleGeoSucces(position) {
    const latitude = position.coords.latitude;
    const longitude = position.coords.longitude;
    const coordsObj = {
-    //  if the name of object and key are the same, just
-
-     /*latitude, 
-       longitude 
-     */
-    //is okay
        latitude: latitude,
        longitude: longitude
    };
    saveCoords(coordsObj);
-   getWeather(latitude, longitude);/////
+   getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -47,10 +41,8 @@ function loadCoords() {
     if(loadedCoords === null){
         askForCoords();
     } else {
-        const parseCoords = JSON.parse(loadedCoords);/////
-        //console.log(parseCoords);/////
+        const parseCoords = JSON.parse(loadedCoords);
         getWeather(parseCoords.latitude,parseCoords.longitude);
-        //getWeather
     }
 }
 
