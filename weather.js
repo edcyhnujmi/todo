6de26bab72dde541f13f5e8fb2e18edff5f6e5cf
edcyhnujmi <43102478+edcyhnujmi@users.noreply.github.com>
2020-07-18 20:33:31 +0900

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,15 +1,23 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";/////
 const COORDS = 'coords';
 
 function getWeather(lat,lng){/////
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`);/////
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`).then(function(response) {
+        return response.json();
+    }).then(function(json) { //then 사용하면 데이터 준비될 때까지 기다리게 할 수 있음
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });/////
 }/////
 
-function saveCoords(coordsObj){
+function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
-function handleGeoSucces(position){
+function handleGeoSucces(position) {
    const latitude = position.coords.latitude;
    const longitude = position.coords.longitude;
    const coordsObj = {
@@ -26,15 +34,15 @@ function handleGeoSucces(position){
    getWeather(latitude, longitude);/////
 }
 
-function handleGeoError(){
+function handleGeoError() {
     console.log("Cant access geo location");
 }
 
-function askForCoords(){
+function askForCoords() {
     navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError)
 }
 
-function loadCoords(){
+function loadCoords() {
     const loadedCoords = localStorage.getItem(COORDS);
     if(loadedCoords === null){
         askForCoords();
@@ -46,7 +54,7 @@ function loadCoords(){
     }
 }
 
-function init(){
+function init() {
     loadCoords();
 }
 
