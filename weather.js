8088931943b72c196311c1e11a0044627e59f50d
edcyhnujmi <43102478+edcyhnujmi@users.noreply.github.com>
2020-07-19 23:56:09 +0900

    Update weather.js

--- weather.js
+++ weather.js
@@ -3,7 +3,7 @@ const weather = document.querySelector(".js-weather");
 const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
 const COORDS = 'coords';
 
-function getWeather(lat,lng){
+function getWeather(lat,lng) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`).then(function(response) {
         return response.json();
     }).then(function(json) {
